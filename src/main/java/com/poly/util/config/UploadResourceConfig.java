0659df20516e97968131168f8b374f9deab18dc2
hungpt3107 <thhung3107@gmail.com>
2025-07-03 10:01:10 +0700

    Fix upload img success

---
+++ src/main/java/com/poly/util/config/UploadResourceConfig.java
@@ -0,0 +1,16 @@
+package com.poly.util.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class UploadResourceConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/uploads/**")
+                .addResourceLocations("file:uploads/"); // thư mục cùng cấp target/
+    }
+}
