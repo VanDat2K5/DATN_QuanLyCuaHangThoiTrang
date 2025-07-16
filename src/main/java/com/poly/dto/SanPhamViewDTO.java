38fe9ea74267d048d0d007dd1e273e8f7200d3db
hungpt3107 <thhung3107@gmail.com>
2025-07-16 16:04:36 +0700

    Load Product

---
+++ src/main/java/com/poly/dto/SanPhamViewDTO.java
@@ -0,0 +1,59 @@
+package com.poly.dto;
+
+import com.poly.entity.SanPham;
+
+import java.math.BigDecimal;
+
+public class SanPhamViewDTO {
+    private String maSP;
+    private String tenSP;
+    private String hinhAnhDauTien;
+    private BigDecimal giaXuat;
+
+    public SanPhamViewDTO(SanPham sp) {
+        this.maSP = sp.getMaSP();
+        this.tenSP = sp.getTenSP();
+
+        if (sp.getHinhAnh() != null && !sp.getHinhAnh().isEmpty()) {
+            this.hinhAnhDauTien = "/images/" + sp.getHinhAnh().get(0).getHinhAnh();
+
+        }
+
+        if (sp.getChiTietSanPham() != null && !sp.getChiTietSanPham().isEmpty()) {
+            this.giaXuat = sp.getChiTietSanPham().get(0).getGiaXuat();
+        }
+    }
+
+    // Getters and Setters
+    public String getMaSP() {
+        return maSP;
+    }
+
+    public void setMaSP(String maSP) {
+        this.maSP = maSP;
+    }
+
+    public String getTenSP() {
+        return tenSP;
+    }
+
+    public void setTenSP(String tenSP) {
+        this.tenSP = tenSP;
+    }
+
+    public String getHinhAnhDauTien() {
+        return hinhAnhDauTien;
+    }
+
+    public void setHinhAnhDauTien(String hinhAnhDauTien) {
+        this.hinhAnhDauTien = hinhAnhDauTien;
+    }
+
+    public BigDecimal getGiaXuat() {
+        return giaXuat;
+    }
+
+    public void setGiaXuat(BigDecimal giaXuat) {
+        this.giaXuat = giaXuat;
+    }
+}
